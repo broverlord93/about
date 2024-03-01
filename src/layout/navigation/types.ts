8de908436033c5a83e0d6c41ec720c1791fc8679
Anthony Limani <alimani9193@gmail.com>
2024-03-01 09:37:07 -0500

    Modify NavLinkProps so that one of label or icon MUST be set

--- src/layout/navigation/types.ts
+++ src/layout/navigation/types.ts
@@ -1,5 +1,12 @@
-export interface NavLinkProps {
+export type NavLinkProps = {
   icon?: () => JSX.Element;
   label?: string;
   path: string;
-}
+} & (
+  | {
+      icon: () => JSX.Element;
+    }
+  | {
+      label: string;
+    }
+);
