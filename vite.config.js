33d2c6a4286128158b43783c43a8c3649eb23106
Anthony Limani <alimani9193@gmail.com>
2024-03-02 06:21:41 -0500

    Add src/layout to path aliases

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,6 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 import sassDts from "vite-plugin-sass-dts";
-import * as paths from "./paths.json";
-
-const computedPaths = () => {
-  const computed = {};
-  for (const key in paths) {
-    const alias = `@${key}`;
-    computed[alias] = path.resolve(__dirname, paths[key]);
-  }
-  return computed;
-};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -29,6 +19,7 @@ export default defineConfig({
     alias: {
       "@assets": path.resolve(__dirname, "src/assets"),
       "@components": path.resolve(__dirname, "src/components"),
+      "@layout": path.resolve(__dirname, "src/layout"),
       "@src": path.resolve(__dirname, "src"),
     },
   },
