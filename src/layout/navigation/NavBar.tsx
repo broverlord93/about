9d06ca0895572c8f44ba35dd069b8bb52a6828d5
Anthony Limani <alimani9193@gmail.com>
2024-03-15 08:54:13 -0400

    Adds logo / branding to the nav bar

--- src/layout/navigation/NavBar.tsx
+++ src/layout/navigation/NavBar.tsx
@@ -1,3 +1,4 @@
+import NavBranding from "@layout/navigation/NavBranding.tsx";
 import React from "react";
 import NavLink from "./NavLink.tsx";
 import { navLinks } from "./navLinks.tsx";
@@ -5,6 +6,9 @@ import { navLinks } from "./navLinks.tsx";
 const NavBar: React.FC = () => {
   return (
     <div className={"nav"}>
+      <div className={"nav-branding"}>
+        <NavBranding className={"nav-branding-canvas"} />
+      </div>
       <ul className={"nav-items"}>
         {navLinks.map(({ icon, label, path }, index) => {
           return (
