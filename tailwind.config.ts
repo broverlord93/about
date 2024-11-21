dbd2b1d1593fa9211f112b78bd3a2427a46c7dfb
Anthony Limani <alimani9193+github@gmail.com>
2024-11-21 17:14:28 -0500

    Add PostCSS and related plugins. Configure PostCSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "media",
+  mode: "jit",
+  plugins: [],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
 } satisfies Config
 
