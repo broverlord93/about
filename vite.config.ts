2d74a7ed505278f55cb34edc3ed2221f7a96deb9
Anthony Limani <alimani9193+github@gmail.com>
2024-11-22 08:45:53 -0500

    Remove SASS plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,19 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import sassDts from "vite-plugin-sass-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    sassDts({
-      enabledMode: ["development", "production"],
-      global: {
-        generate: true,
-        outputFilePath: path.resolve(__dirname, "./src/@types/style.d.ts")
-      }
-    })
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@assets": path.resolve(__dirname, "src/assets"),
       "@components": path.resolve(__dirname, "src/components"),
       "@layout": path.resolve(__dirname, "src/layout"),
       "@redux": path.resolve(__dirname, "src/redux"),
-      "@src": path.resolve(__dirname, "src")
-    }
+      "@src": path.resolve(__dirname, "src"),
+    },
   },
   server: {
-    port: 8000
-  }
+    port: 8000,
+  },
 });
