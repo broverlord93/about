251ee154aaee2a490b7325987087fda918f72460
Anthony Limani <alimani9193+github@gmail.com>
2024-11-22 21:11:04 -0500

    Rename to index, fix imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import { store } from "@redux/store.ts";
+import { store } from "@redux/store";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -10,5 +10,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
