479e7f8e8770b91f69d4271b1e6244490a0f0e2c
Anthony Limani <alimani9193+github@gmail.com>
2024-11-25 17:52:16 -0500

    Creates page layout for base page

---
+++ src/app/Layout.tsx
@@ -0,0 +1,22 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import AppSidebar from "@components/app-sidebar";
+import type { FC, ReactNode } from "react";
+import Main from "./Main";
+
+interface LayoutProps {
+  children?: ReactNode;
+}
+
+const Layout: FC<LayoutProps> = ({ children }) => {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <Main>
+        <SidebarTrigger />
+        {children}
+      </Main>
+    </SidebarProvider>
+  );
+};
+
+export default Layout;
