37ace51ed2f92c4afa062520715332380f556b3b
Anthony Limani <alimani9193+github@gmail.com>
2024-11-25 17:51:37 -0500

    Creates a basic sidebar component

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,70 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Brand from "@app/Brand";
+import { Home, Settings } from "lucide-react";
+import type { FC } from "react";
+import GithubIcon from "./icons/GithubIcon";
+import LinkedInIcon from "./icons/LinkedInIcon";
+
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Github",
+    url: "https://github.com/broverlord93",
+    icon: GithubIcon,
+  },
+  {
+    title: "LinkedIn",
+    url: "https://www.linkedin.com/in/anthonylimani",
+    icon: LinkedInIcon,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+const AppSidebar: FC = () => {
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <Brand className={"brand"} />
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel></SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon className={"h-2 w-2"} />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+};
+
+export default AppSidebar;
