31345d62ca121db07e3dd6ead9ce4d22ae3443c1
Anthony Limani <alimani9193+github@gmail.com>
2024-11-25 17:50:21 -0500

    Remove buttons now that the styles are where I want them

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,18 +1,7 @@
-import { Button } from "@components/ui/button";
 import { FC, PropsWithChildren } from "react";
 
 const Main: FC<PropsWithChildren> = ({ children }) => {
-  return (
-    <div className={"bg-lemon-chiffon-50 h-dvh"}>
-      {children}
-      <Button variant={"default"}>Default</Button>
-      <Button variant={"destructive"}>Destructive</Button>
-      <Button variant={"outline"}>Outline</Button>
-      <Button variant={"secondary"}>Secondary</Button>
-      <Button variant={"ghost"}>Ghost</Button>
-      <Button variant={"link"}>Link</Button>
-    </div>
-  );
+  return <div className={"bg-lemon-chiffon-100 h-dvh w-dvw"}>{children}</div>;
 };
 
 export default Main;
