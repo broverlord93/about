96e76e311349dfe726e752adf3029ff3bbc080c1
Anthony Limani <alimani9193+github@gmail.com>
2024-12-03 16:33:14 -0500

    Tweak spacing

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,19 +1,14 @@
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@components/ui/card.js";
+import { Card, CardContent, CardHeader, CardTitle } from "@components/ui/card";
 import { FC, PropsWithChildren } from "react";
 
 const Main: FC<PropsWithChildren> = ({ children }) => {
   return (
     <main
       className={
-        "fixed left-[var(--sidebar-width)] top-[var(--header-height)] h-[calc(100vh_-_var(--header-height))] w-[calc(100vw_-_var(--sidebar-width))]"
+        "fixed left-[var(--sidebar-width)] top-[var(--header-height)] h-[calc(100vh_-_var(--header-height))] w-[calc(100vw_-_var(--sidebar-width))] px-5 py-2"
       }
     >
-      <Card className={"bg-lemon-chiffon-50 mx-5 my-5 h-dvh shadow-md"}>
+      <Card className={"bg-lemon-chiffon-50 h-full shadow-md"}>
         <CardHeader>
           <CardTitle></CardTitle>
         </CardHeader>
