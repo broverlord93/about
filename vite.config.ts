30894177f887aec1ad22d989c531201eda43aac9
Anthony Limani <alimani9193+github@gmail.com>
2024-12-18 09:01:22 -0500

    Add tanstack router with devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [TanStackRouterVite(), react()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
