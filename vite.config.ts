56455b8b308cef82a6aec396fed125cfc1b6324a
Anthony Limani <alimani9193+github@gmail.com>
2025-01-31 08:53:21 -0500

    Add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -16,9 +16,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
       "@assets": path.resolve(__dirname, "src/assets"),
       "@components": path.resolve(__dirname, "src/components"),
+      "@lib": path.resolve(__dirname, "src/lib"),
       "@redux": path.resolve(__dirname, "src/redux"),
       "@routes": path.resolve(__dirname, "src/routes"),
       "@src": path.resolve(__dirname, "src"),
+      "@types": path.resolve(__dirname, "src/types"),
     },
   },
   server: {
