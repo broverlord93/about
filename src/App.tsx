b141477ce34b7047a4e439e072fdc6c5fef05ae2
Anthony Limani <alimani9193+github@gmail.com>
2025-01-31 08:56:09 -0500

    Create router and pass to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
 import Layout from "@/Layout";
+import { Spinner } from "@components/Spinner";
+import { auth } from "@lib/auth";
+import {
+  createRouter,
+  ErrorComponent,
+  RouterProvider,
+} from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 
-function App() {
-  return <Layout></Layout>;
+const router = createRouter({
+  routeTree,
+  defaultPendingComponent: () => (
+    <div className={`p-2 text-2xl`}>
+      <Spinner />
+    </div>
+  ),
+  defaultErrorComponent: ({ error }) => <ErrorComponent error={error} />,
+  context: {
+    auth: undefined!, // We'll inject this when we render
+  },
+  defaultPreload: "intent",
+  scrollRestoration: true,
+});
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
 }
+
+const App = () => {
+  return (
+    <Layout>
+      <RouterProvider
+        router={router}
+        defaultPreload="intent"
+        context={{
+          auth,
+        }}
+      />
+    </Layout>
+  );
+};
 
 export default App;
