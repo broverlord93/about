a69f16afd6db43bd2542d3a972d638365e61fc3d
Anthony Limani <alimani9193+github@gmail.com>
2025-02-04 09:29:45 -0500

    Add types for timeline styles

--- src/Timeline/timeline.ts
+++ src/Timeline/timeline.ts
@@ -1,22 +1,7 @@
-// import PropTypes from "prop-types";
-// import type { ReactNode } from "react";
-//
-// // generic types
-// import type { colors } from "../generic";
-// import { propTypesColors } from "../generic";
-//
-// /**
-//  * This file contains the types and prop-types for Timeline components.
-//  */
-//
-// // typescript types
-// export type className = string;
-// export type children = ReactNode;
-// export type color = "white" | colors;
-// export type variant = "filled" | "outlined" | "ghost" | "gradient";
-//
-// // javascript prop-types
-// export const propTypeClassName: any = PropTypes.string;
-// export const propTypeChildren: any = PropTypes.node;
-// export const propTypeColor: any = ["white", ...propTypesColors];
-// export const propTypeVariant: any = ["filled", "outlined", "ghost", "gradient"];
+import type { colors } from "@type-defs/style";
+import type { ReactNode } from "react";
+
+export type className = string;
+export type children = ReactNode;
+export type color = "white" | colors;
+export type variant = "filled" | "outlined" | "ghost" | "gradient";
