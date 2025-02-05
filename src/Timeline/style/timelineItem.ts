f838a4f852787b7d38fbe5f99176758ce46444d2
Anthony Limani <alimani9193+github@gmail.com>
2025-02-05 17:25:04 -0500

    Remove superfluous type fef

--- src/Timeline/style/timelineItem.ts
+++ src/Timeline/style/timelineItem.ts
@@ -1,20 +1,4 @@
-import { color, variant } from "../types";
-
-export interface TimelineItemStyleTypes {
-  defaultProps?: {
-    color?: color;
-    variant?: variant;
-  };
-  valid?: {
-    colors?: string[];
-    variants?: string[];
-  };
-  styles?: {
-    base?: object;
-  };
-}
-
-export const timelineItem: TimelineItemStyleTypes = {
+export const timelineItem = {
   defaultProps: {
     color: "gray",
     variant: "filled",
