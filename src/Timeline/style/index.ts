adde421ed325ffc370b60b9c5672c594acd1aba1
Anthony Limani <alimani9193+github@gmail.com>
2025-02-05 17:30:05 -0500

    Move code from deleted files to index

--- src/Timeline/style/index.ts
+++ src/Timeline/style/index.ts
@@ -1,6 +1,90 @@
-export * from "./timeline";
-export * from "./timelineItem";
-export * from "./timelineIcon";
-export * from "./timelineHeader";
-export * from "./timelineBody";
-export * from "./timelineConnector";
+import { filled, ghost, gradient, outlined } from "./colors";
+
+export const timeline = {
+  styles: {
+    base: {
+      display: "w-full",
+      position: "flex",
+      flexDirection: "flex-col",
+    },
+  },
+};
+
+export const timelineBody = {
+  styles: {
+    base: {
+      display: "flex",
+      gap: "gap-4",
+    },
+  },
+};
+
+export const timelineConnector = {
+  styles: {
+    base: {
+      container: {
+        position: "absolute",
+        left: "left-0",
+        display: "grid",
+        justifyContent: "justify-center",
+        backgroundColor: "bg-transparent",
+        transition: "transition-opacity duration-200",
+      },
+      line: {
+        width: "w-0.5",
+        height: "h-full",
+        backgroundColor: "bg-blue-gray-100",
+      },
+    },
+  },
+};
+
+export const timelineHeader = {
+  styles: {
+    base: {
+      display: "flex",
+      alignItems: "items-center",
+      gap: "gap-4",
+    },
+  },
+};
+
+export const timelineIcon = {
+  defaultProps: {
+    color: "gray",
+    variant: "filled",
+  },
+  styles: {
+    base: {
+      width: "w-max",
+      height: "w-max",
+      padding: "p-1.5",
+      position: "relative",
+      zIndex: "z-[2]",
+      flexShrink: "flex-shrink-0",
+      borderRadius: "rounded-full",
+      overflow: "overflow-hidden",
+    },
+    variants: {
+      ghost,
+      filled,
+      outlined,
+      gradient,
+    },
+  },
+};
+
+export const timelineItem = {
+  defaultProps: {
+    color: "gray",
+    variant: "filled",
+  },
+  styles: {
+    base: {
+      display: "flex",
+      position: "relative",
+      flexDirection: "flex-col",
+      gap: "gap-2",
+    },
+  },
+};
