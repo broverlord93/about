2844b003e507a52df120c56ebb794d84a53e5d2a
Anthony Limani <alimani9193+github@gmail.com>
2025-02-11 09:24:00 -0500

    Refactor DateTimePicker for tailwind

--- src/components/ui/DateTimePicker.tsx
+++ src/components/ui/DateTimePicker.tsx
@@ -1,52 +1,56 @@
-// import { mdiCalendarClock } from "@mdi/js";
-// import Icon from "@mdi/react";
-// import flatpickr from "flatpickr";
-// import { type FC, type ReactNode, useCallback } from "react";
-// import Flatpickr, { type DateTimePickerProps } from "react-flatpickr";
-// import { Input, InputGroup, InputGroupText, type InputProps } from "reactstrap";
-// import Hook = flatpickr.Options.Hook;
-//
-// interface Props
-//   extends Omit<DateTimePickerProps, "children" | "onChange" | "render"> {
-//   onChange: Hook;
-//   inputProps?: InputProps;
-//   useIcon?: boolean | ReactNode;
-// }
-//
-// export const DateTimePicker: FC<Props> = ({
-//   inputProps,
-//   onChange,
-//   options,
-//   useIcon = true,
-//   ...rest
-// }) => {
-//   const getIcon = useCallback(() => {
-//     if (useIcon) {
-//       return (
-//         <InputGroupText>
-//           <span className={"text-primary"}>
-//             <Icon path={mdiCalendarClock} size={1} />
-//           </span>
-//         </InputGroupText>
-//       );
-//     }
-//
-//     return <>{useIcon && <InputGroupText>{useIcon}</InputGroupText>}</>;
-//   }, [useIcon]);
-//
-//   return (
-//     <Flatpickr
-//       onChange={onChange}
-//       options={{ dateFormat: "F j, Y H:i", enableTime: true, ...options }}
-//       render={({}, ref) => (
-//         <InputGroup>
-//           {getIcon()}
-//           <Input className={"bg-white"} innerRef={ref} {...inputProps} />
-//         </InputGroup>
-//       )}
-//       {...rest}
-//     />
-//   );
-// };
-//
-// export default DateTimePicker;
+import { Input } from "@components/ui/input";
+import flatpickr from "flatpickr";
+import { CalendarClock } from "lucide-react";
+import { ComponentProps, type FC, type ReactNode } from "react";
+import Flatpickr, { type DateTimePickerProps } from "react-flatpickr";
+import Hook = flatpickr.Options.Hook;
+
+type UseIconProps =
+  | {
+      useIcon: true;
+      Icon?: never;
+    }
+  | {
+      useIcon?: false;
+      Icon?: ReactNode;
+    };
+
+interface Props
+  extends Omit<DateTimePickerProps, "children" | "onChange" | "render"> {
+  onChange: Hook;
+  inputProps?: ComponentProps<"input">;
+}
+
+export const DateTimePicker: FC<Props & UseIconProps> = ({
+  Icon,
+  inputProps,
+  onChange,
+  options,
+  useIcon = true,
+  ...rest
+}) => (
+  <Flatpickr
+    onChange={onChange}
+    options={{ dateFormat: "F j, Y H:i", enableTime: true, ...options }}
+    render={(_props, ref) => (
+      <div className="relative mb-4 flex items-stretch">
+        {(useIcon || Icon) && (
+          <span
+            className="text-surface dark:border-white/10 flex items-center whitespace-nowrap rounded-l-md border border-b border-l border-r-0 border-t border-solid border-lemon-chiffon-900 px-3 py-[0.25rem] text-center text-base font-normal leading-[1.6] dark:text-lemon-chiffon-900"
+            id="basic-addon1"
+          >
+            {useIcon ? <CalendarClock /> : Icon}
+          </span>
+        )}
+        <Input
+          className={"rounded-none rounded-r-md"}
+          ref={ref}
+          {...inputProps}
+        />
+      </div>
+    )}
+    {...rest}
+  />
+);
+
+export default DateTimePicker;
