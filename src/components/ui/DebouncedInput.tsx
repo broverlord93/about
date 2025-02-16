75cf3d9daef06038568ccb85581e9df9a7f90a85
Anthony Limani <alimani9193+github@gmail.com>
2025-02-16 14:58:03 -0500

    Update DebouncedInput implementation

--- src/components/ui/DebouncedInput.tsx
+++ src/components/ui/DebouncedInput.tsx
@@ -1,35 +1,29 @@
-// import useInput from "@src/hooks/useInput";
-// import { useEffect } from "react";
-// import { Input, type InputProps } from "reactstrap";
-// import { SearchTracker } from "@helpers/tracking_template";
-//
-// const DebouncedInput = ({
-//   searchLocation = "",
-//   searchType = "",
-//   value: initialValue,
-//   onChange,
-//   debounce = 500,
-//   ...props
-// }: {
-//   searchLocation?: string;
-//   searchType?: string;
-//   value: string | number;
-//   onChange: (value: string | number) => void;
-//   debounce?: number;
-// } & Omit<InputProps, "onChange">) => {
-//   const { debouncedValue, onChangeHandler, value } = useInput({
-//     debounce,
-//     initialValue,
-//   });
-//
-//   useEffect(() => {
-//     onChange(debouncedValue);
-//     if (debouncedValue) {
-//       SearchTracker(debouncedValue, searchType, searchLocation);
-//     }
-//   }, [debouncedValue]);
-//
-//   return <Input {...props} value={value} onChange={onChangeHandler} />;
-// };
-//
-// export default DebouncedInput;
+import { Input } from "@components/ui/input";
+import useInput from "@src/hooks/useInput";
+import { ComponentProps, FC, useEffect } from "react";
+
+interface DebouncedInputProps extends ComponentProps<"input"> {
+  value: string | number;
+  onDebouncedChange: (value: string | number) => void;
+  debounce?: number;
+}
+
+const DebouncedInput: FC<DebouncedInputProps> = ({
+  value: initialValue,
+  onDebouncedChange,
+  debounce = 500,
+  ...props
+}) => {
+  const { debouncedValue, onChangeHandler, value } = useInput({
+    debounce,
+    initialValue,
+  });
+
+  useEffect(() => {
+    onDebouncedChange(debouncedValue);
+  }, [debouncedValue, onDebouncedChange]);
+
+  return <Input value={value} onChange={onChangeHandler} {...props} />;
+};
+
+export default DebouncedInput;
