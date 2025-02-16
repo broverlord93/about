a50d20103fd3198278db028c02cbfb97fddfb4cf
Anthony Limani <alimani9193+github@gmail.com>
2025-02-16 15:59:17 -0500

    Update DefaultExpanderColumn implementation

--- src/table/DefaultExpanderColumn.tsx
+++ src/table/DefaultExpanderColumn.tsx
@@ -1,42 +1,37 @@
-// import { Row as TableRow, Table } from "@tanstack/react-table";
-// import { ChevronDown, ChevronRight } from "react-feather";
-// import { Row } from "reactstrap";
-//
-// const DefaultExpanderColumn = <TData,>() => {
-//   return {
-//     id: "expander",
-//     cell: ({
-//              row: { getIsExpanded, toggleExpanded },
-//            }: {
-//       row: TableRow<TData>;
-//     }) => {
-//       return (
-//         <>
-//           {
-//         <Row className={"cursor-pointer"} onClick={() => toggleExpanded()}>
-//       {getIsExpanded() ? <ChevronDown /> : <ChevronRight />}
-//       </Row>
-//     }
-//       </>
-//     );
-//     },
-//     enableColumnFilter: false,
-//     header: ({
-//                table: { getIsAllRowsExpanded, getToggleAllRowsExpandedHandler },
-//              }: {
-//       table: Table<TData>;
-//     }) => {
-//       return (
-//         <Row
-//           className={"cursor-pointer"}
-//       onClick={getToggleAllRowsExpandedHandler()}
-//       >
-//       {getIsAllRowsExpanded() ? <ChevronDown /> : <ChevronRight />}
-//       </Row>
-//     );
-//     },
-//     size: 75,
-//   };
-// };
-//
-// export default DefaultExpanderColumn;
+import { Row, Table } from "@tanstack/react-table";
+import { ChevronDown, ChevronRight } from "lucide-react";
+
+const DefaultExpanderColumn = <TData,>() => {
+  return {
+    id: "expander",
+    cell: ({ row: { getIsExpanded, toggleExpanded } }: { row: Row<TData> }) => {
+      return (
+        <>
+          {
+            <div className={"cursor-pointer"} onClick={() => toggleExpanded()}>
+              {getIsExpanded() ? <ChevronDown /> : <ChevronRight />}
+            </div>
+          }
+        </>
+      );
+    },
+    enableColumnFilter: false,
+    header: ({
+      table: { getIsAllRowsExpanded, getToggleAllRowsExpandedHandler },
+    }: {
+      table: Table<TData>;
+    }) => {
+      return (
+        <div
+          className={"cursor-pointer"}
+          onClick={getToggleAllRowsExpandedHandler()}
+        >
+          {getIsAllRowsExpanded() ? <ChevronDown /> : <ChevronRight />}
+        </div>
+      );
+    },
+    size: 75,
+  };
+};
+
+export default DefaultExpanderColumn;
