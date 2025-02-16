35119850a171566a8f332ba9733e2cbd8443af38
Anthony Limani <alimani9193+github@gmail.com>
2025-02-16 14:31:39 -0500

    Reimpliment DefaultCheckboxColumn

--- src/table/DefaultCheckboxColumn.tsx
+++ src/table/DefaultCheckboxColumn.tsx
@@ -1,67 +1,57 @@
-// import type { Row as TableRow, Table } from "@tanstack/react-table";
-// import { Col, Row } from "reactstrap";
-// import IndeterminateCheckbox from "./IndeterminateCheckbox";
-//
-// const DefaultCheckboxColumn = <TData,>({
-//   isPaginationEnabled = false,
-// }: {
-//   isPaginationEnabled: boolean;
-// }) => {
-//   return {
-//     id: "select",
-//     ...(isPaginationEnabled && {
-//       footer: ({ table }: { table: Table<TData> }) => {
-//         const { pagination } = table.getState();
-//
-//         return (
-//           <Row className={"justify-content-start align-items-center"}>
-//             <Col sm={3}>
-//               <IndeterminateCheckbox
-//                 {...{
-//                   checked: table.getIsAllPageRowsSelected(),
-//                   indeterminate: table.getIsSomePageRowsSelected(),
-//                   onChange: table.getToggleAllPageRowsSelectedHandler(),
-//                 }}
-//               />
-//             </Col>
-//             <Col sm={9}>
-//               <span className={"text-start align-middle"}>
-//                 Select All <br />
-//                 {pagination?.pageSize && `Page Rows (${pagination.pageSize})`}
-//               </span>
-//             </Col>
-//           </Row>
-//         );
-//       },
-//     }),
-//     header: ({ table }: { table: Table<TData> }) => (
-//       <Row className={"justify-content-start align-items-center"}>
-//         <Col>
-//           <IndeterminateCheckbox
-//             {...{
-//               checked: table.getIsAllRowsSelected(),
-//               indeterminate: table.getIsSomeRowsSelected(),
-//               onChange: table.getToggleAllRowsSelectedHandler(),
-//             }}
-//           />
-//         </Col>
-//       </Row>
-//     ),
-//     cell: ({ row }: { row: TableRow<TData> }) => (
-//       <Row className={"justify-content-start align-items-center"}>
-//         <Col>
-//           <IndeterminateCheckbox
-//             {...{
-//               checked: row.getIsSelected(),
-//               disabled: !row.getCanSelect(),
-//               onChange: row.getToggleSelectedHandler(),
-//             }}
-//           />
-//         </Col>
-//       </Row>
-//     ),
-//     size: isPaginationEnabled ? 250 : 100,
-//   };
-// };
-//
-// export default DefaultCheckboxColumn;
+import IndeterminateCheckbox from "@components/ui/IndeterminateCheckbox";
+import type { Row as TableRow, Table } from "@tanstack/react-table";
+
+const DefaultCheckboxColumn = <TData,>({
+  isPaginationEnabled = false,
+}: {
+  isPaginationEnabled: boolean;
+}) => {
+  return {
+    id: "select",
+    ...(isPaginationEnabled && {
+      footer: ({ table }: { table: Table<TData> }) => {
+        const { pagination } = table.getState();
+        const checked = table.getIsSomePageRowsSelected()
+          ? "indeterminate"
+          : table.getIsAllPageRowsSelected();
+
+        return (
+          <div>
+            <IndeterminateCheckbox
+              checked={checked}
+              onCheckedChange={table.getToggleAllPageRowsSelectedHandler()}
+            />
+            <span className={"text-start align-middle"}>
+              Select All <br />
+              {pagination?.pageSize && `Page Rows (${pagination.pageSize})`}
+            </span>
+          </div>
+        );
+      },
+    }),
+    header: ({ table }: { table: Table<TData> }) => (
+      <div>
+        <IndeterminateCheckbox
+          checked={
+            table.getIsSomeRowsSelected()
+              ? "indeterminate"
+              : table.getIsAllRowsSelected()
+          }
+          onCheckedChange={table.getToggleAllRowsSelectedHandler()}
+        />
+      </div>
+    ),
+    cell: ({ row }: { row: TableRow<TData> }) => (
+      <div>
+        <IndeterminateCheckbox
+          checked={row.getIsSelected()}
+          disabled={!row.getCanSelect()}
+          onCheckedChange={row.getToggleSelectedHandler()}
+        />
+      </div>
+    ),
+    size: isPaginationEnabled ? 250 : 100,
+  };
+};
+
+export default DefaultCheckboxColumn;
