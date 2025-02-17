900caf54f58c76562cbdb87ab2d0393f456c90df
Anthony Limani <alimani9193+github@gmail.com>
2025-02-17 08:41:36 -0500

    Update styling for Shadcn select

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -16,7 +16,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "bg-white ring-offset-white flex h-10 w-full items-center justify-between rounded-md border border-husk-200 px-3 py-2 text-sm placeholder:text-lemon-chiffon-500 focus:outline-none focus:ring-2 focus:ring-lemon-chiffon-950 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-husk-800 dark:bg-lemon-chiffon-950 dark:ring-offset-lemon-chiffon-950 dark:placeholder:text-lemon-chiffon-400 dark:focus:ring-lemon-chiffon-300 [&>span]:line-clamp-1",
+      "bg-white ring-offset-white flex h-10 w-full items-center justify-between rounded-md border border-husk-200 px-3 py-2 text-sm text-lemon-chiffon-900 placeholder:text-lemon-chiffon-500 focus:outline-none focus:ring-2 focus:ring-lemon-chiffon-950 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-husk-800 dark:bg-lemon-chiffon-950 dark:ring-offset-lemon-chiffon-950 dark:placeholder:text-lemon-chiffon-400 dark:focus:ring-lemon-chiffon-300 [&>span]:line-clamp-1",
       className,
     )}
     {...props}
@@ -72,7 +72,7 @@ const SelectContent = React.forwardRef<
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
-        "bg-white relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border border-husk-200 text-lemon-chiffon-950 shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 dark:border-husk-800 dark:bg-lemon-chiffon-950 dark:text-lemon-chiffon-50",
+        "bg-white relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border border-husk-200 text-lemon-chiffon-900 shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 dark:border-husk-800 dark:bg-lemon-chiffon-950 dark:text-lemon-chiffon-50",
         position === "popper" &&
           "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
         className,
