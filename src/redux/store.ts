c68abf673d31bf8cf0bafe0a00f660fb14cab536
Anthony Limani <alimani9193+github@gmail.com>
2025-09-20 10:37:11 -0400

    Adds a dummy slice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+// Create a simple root reducer since you don't have any slices yet
+const rootReducer = {
+  // Add your slices here when you create them
+  // For now, we'll use a placeholder to prevent the empty reducer error
+  _placeholder: (state = {}) => state,
+}
+
 export const store = configureStore({
-  reducer: {}
+  reducer: rootReducer
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
