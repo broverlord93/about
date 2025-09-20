259f4405565d86fbdf7b171901abd0b001662853
Anthony Limani <alimani9193+github@gmail.com>
2025-09-20 10:37:51 -0400

    Gut comments

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
 
-// Create a simple root reducer since you don't have any slices yet
 const rootReducer = {
-  // Add your slices here when you create them
-  // For now, we'll use a placeholder to prevent the empty reducer error
   _placeholder: (state = {}) => state,
-}
+};
 
 export const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
